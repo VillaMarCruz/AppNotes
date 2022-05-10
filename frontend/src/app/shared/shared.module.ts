5670aa30e6d4d722e91099ed8b468b516c6e6e3c
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-10 16:50:53 -0500

    Modulos compartidos

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -12,6 +12,9 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SideBarComponent
   ]
 })
 export class SharedModule { }
