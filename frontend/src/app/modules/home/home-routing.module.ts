ba4835cf63715ded2f55428c637413941a3a9ab8
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-05-16 12:37:17 -0500

    Molules - Routes

--- frontend/src/app/modules/home/home-routing.module.ts
+++ frontend/src/app/modules/home/home-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
+
 const routes: Routes = [
   {
-    path:'', // TODO: 
+    path:'',
     component: HomePageComponent
+
   }
 ];
 
